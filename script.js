f389f8b20f50266ae86d5b346c15b9a0e715755f
Ray <sewellray11+git@googlemail.com>
2021-07-15 16:51:35 +0100

    added check for draw, input for player names, re/start game button

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const boardGrid = document.querySelector("#board");
 const message = document.querySelector("#message");
+const player1Name = document.querySelector("#player1");
+const player2Name = document.querySelector("#player2");
 const winningPositions = [
     [0,1,2],
     [3,4,5],
@@ -10,7 +12,10 @@ const winningPositions = [
     [0,4,8],
     [2,4,6]
 ];
-let spaces;
+
+let game;
+let player1;
+let player2;
 
 class Game {
     constructor(player1, player2) {
@@ -28,34 +33,37 @@ class Game {
         })
     }
     applyOnClickToSpaces() {
-        spaces = document.querySelectorAll(".space");
+        let spaces = document.querySelectorAll(".space");
         spaces.forEach((space, i) => {
             space.onclick = function() {game.playerMove(space)};
             space.dataset.pos = i;
         })
     }
     playerMove(space) {
-        if (!space.innerHTML) {
-            switch(this.gameState) {
-                case "p1":
-                    space.innerHTML = this.player1.symbol;
-                    space.style.backgroundColor = "darkturquoise";
-                    break;
-                case "p2":
-                    space.innerHTML = this.player2.symbol;
-                    space.style.backgroundColor = "tomato";
-                    break;
-                default:
-                    message.innerHTML = "The game is already over!";
-                    break;
+        if (this.gameState != "end") {
+            if (!space.innerHTML) {
+                switch(this.gameState) {
+                    case "p1":
+                        space.innerHTML = this.player1.symbol;
+                        space.style.backgroundColor = "darkturquoise";
+                        break;
+                    case "p2":
+                        space.innerHTML = this.player2.symbol;
+                        space.style.backgroundColor = "tomato";
+                        break;
+                    default:
+                        message.innerHTML = "The game is already over!";
+                        break;
+                }
+                this.board[space.dataset.pos] = space.innerHTML;
+                this.checkGameState();
+            } else {
+                message.innerHTML = "This space is taken!";
             }
-            this.board[space.dataset.pos] = space.innerHTML;
-            this.checkGameState();
-        } else {
-            message.innerHTML = "This space is taken!";
         }
     }
     checkGameState() {
+        this.checkDraw();
         this.checkWin();
         switch(this.gameState) {
             case "init":
@@ -66,11 +74,11 @@ class Game {
                 break;
             case "p1":
                 this.gameState = "p2";
-                message.innerHTML = "Player 2's turn!";
+                message.innerHTML = player2.name + "'s turn!";
                 break;
             case "p2":
                 this.gameState = "p1";
-                message.innerHTML = "Player 1's turn!";
+                message.innerHTML = player1.name + "'s turn!";
                 break;
             case "end":
                 break;
@@ -84,13 +92,20 @@ class Game {
             });
             if (transcribe.toString() == "X,X,X") {
                 this.gameState = "end";
-                message.innerHTML = "Game over! X wins!";
+                message.innerHTML = "Game over! " + player1.name + " wins!";
             } else if (transcribe.toString() == "O,O,O"){
                 this.gameState = "end";
-                message.innerHTML = "Game over! O wins!";
+                message.innerHTML = "Game over! " + player2.name + " wins!";
             }
             transcribe = [];
         });
+    }
+    checkDraw() {
+        let filteredBoard = this.board.filter(Boolean);
+        if (filteredBoard.length == 9) {
+            this.gameState = "end";
+            message.innerHTML = "Draw!";
+        }
     }
 }
 
@@ -101,7 +116,10 @@ class Player {
     }
 }
 
-let player1 = new Player("Player 1", "X");
-let player2 = new Player("Player 2", "O");
-let game = new Game(player1, player2);
-game.checkGameState();
+function startNewGame() {
+    boardGrid.innerHTML = "";
+    player1 = new Player(player1Name.value, "X");
+    player2 = new Player(player2Name.value, "O");
+    game = new Game(player1, player2);
+    game.checkGameState();
+}
