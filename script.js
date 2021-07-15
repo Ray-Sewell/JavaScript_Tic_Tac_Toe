63b7e1a4677fd820ab8b0e101623cadb214af772
Ray <sewellray11+git@googlemail.com>
2021-07-15 16:29:03 +0100

    check for winning positions, end game if win detected

--- script.js
+++ script.js
@@ -1,28 +1,37 @@
 const boardGrid = document.querySelector("#board");
 const message = document.querySelector("#message");
+const winningPositions = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+];
 let spaces;
 
 class Game {
     constructor(player1, player2) {
-        this.matrix = [[null,null,null],[null,null,null],[null,null,null]];
+        this.board = [null, null, null, null, null, null, null, null, null];
         this.gameState = "init";
         this.player1 = player1;
         this.player2 = player2;
     }
     refreshBoard() {
-        this.matrix.forEach(row => {
-            row.forEach(i => {
-                let space = document.createElement("div");
-                space.className = "space";
-                space.innerHTML = i;
-                boardGrid.appendChild(space);
-            })
+        this.board.forEach(i => {
+            let space = document.createElement("div");
+            space.className = "space";
+            space.innerHTML = i;
+            boardGrid.appendChild(space);
         })
     }
     applyOnClickToSpaces() {
         spaces = document.querySelectorAll(".space");
-        spaces.forEach(space => {
+        spaces.forEach((space, i) => {
             space.onclick = function() {game.playerMove(space)};
+            space.dataset.pos = i;
         })
     }
     playerMove(space) {
@@ -40,12 +49,14 @@ class Game {
                     message.innerHTML = "The game is already over!";
                     break;
             }
+            this.board[space.dataset.pos] = space.innerHTML;
             this.checkGameState();
         } else {
             message.innerHTML = "This space is taken!";
         }
     }
     checkGameState() {
+        this.checkWin();
         switch(this.gameState) {
             case "init":
                 this.refreshBoard();
@@ -65,6 +76,22 @@ class Game {
                 break;
         }
     }
+    checkWin() {
+        let transcribe = [];
+        winningPositions.forEach((pos) => {
+            pos.forEach((i) => {
+                transcribe.push(this.board[i]);
+            });
+            if (transcribe.toString() == "X,X,X") {
+                this.gameState = "end";
+                message.innerHTML = "Game over! X wins!";
+            } else if (transcribe.toString() == "O,O,O"){
+                this.gameState = "end";
+                message.innerHTML = "Game over! O wins!";
+            }
+            transcribe = [];
+        });
+    }
 }
 
 class Player {
@@ -73,8 +100,6 @@ class Player {
         this.symbol = symbol;
     }
 }
-
-
 
 let player1 = new Player("Player 1", "X");
 let player2 = new Player("Player 2", "O");
